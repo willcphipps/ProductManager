2e73f8e8a9d4f4ed3c6999233ddbdfbd1ccbc21d
William Phipps <willcphipps@gmail.com>
2020-05-20 13:07:55 -0500

    adding style

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,27 @@
 import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {Router, Link} from "@reach/router";
 import axios from "axios";
+import Form from './components/Form';
 
 
 function App() {
-  const [product, setProduct] = useState("");
-  const [price, setPrice] = useState();
-  const [description, setDescription] = useState("");
-  const [errors, setErrors] = useState({});
-
-  const addProduct = (e) => {
-    e.preventDefault();
-    let Product = { product, price, description }
-    
-    axios.post("http://localhost:8000/api/products/", Product)
-      .then(res => {
-        console.log(res);
-        if (res.data.errors) {
-          setErrors(res.data.errors);
-        }
-      })
-      .catch(err => console.log(err));
-  }
-
+  const [active, setActive] = useState("/")
   return (
     <div className="App">
       <div className="flex-column">
-        <form onSubmit={ (e) => addProduct(e) } className="form-group col-3">
-          <input className="form-control" type="text"  onChange = {(e)=>setProduct(e.target.value)}/>
-          {errors.product ?
-            <p className="text-danger">{errors.product.message}</p>
-            : ""}
-          <input className="form-control" type="number" onChange={(e) => setPrice(e.target.value)} />
-          {errors.price ?
-            <p className="text-danger">{errors.price.message}</p>
-            : ""}
-          <input className="form-control" type="text" onChange={(e) => setDescription(e.target.value)} />
-          {errors.description ?
-            <p className="text-danger">{errors.description.message}</p>
-            : ""}
-          <input className="btn btn-outline-dark" type="submit"/>
-        </form>
+        <ul className="nav nav-tabs mb-5">
+        <li className="nav-item" onClick={e => setActive("/")} >
+          <Link className={ active === "/" ? "nav-link active" : "nav-link" } to="/">Home</Link>
+        </li>
+        <li className="nav-item" onClick={e => setActive("/new")}>
+          <Link className={ active === "/new" ? "nav-link active" : "nav-link" } to="/new">New Product</Link>
+        </li>
+      </ul>
+        <Router>
+
+         <Form path="/new" />
+        </Router>
       </div>
     </div>
   );
---
+++ client/src/components/Form.jsx
@@ -0,0 +1,50 @@
+  
+import React, { useState } from "react";
+import axios from "axios";
+import { navigate } from "@reach/router";
+
+
+
+const Form = (props) => {
+  const [product, setProduct] = useState("");
+  const [price, setPrice] = useState();
+  const [description, setDescription] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const addProduct = (e) => {
+    e.preventDefault();
+    let Product = { product, price, description }
+    
+    axios.post("http://localhost:8000/api/products/", Product)
+      .then(res => {
+        console.log(res);
+        if (res.data.errors) {
+          setErrors(res.data.errors);
+        }
+      })
+      .catch(err => console.log(err));
+  }
+  return (
+    <div>
+      <h3 className="p-4 my-5 mx-2 text-center bg-secondary">Add New Product</h3> 
+      <div className="d-flex justify-content-center">
+        <form onSubmit={ (e) => addProduct(e) } className="form-group col-3">
+          <input className="m-2 form-control" type="text"  onChange = {(e)=>setProduct(e.target.value)}/>
+          {errors.product ?
+            <p className="text-danger">{errors.product.message}</p>
+            : ""}
+          <input className="m-2 form-control" type="number" onChange={(e) => setPrice(e.target.value)} />
+          {errors.price ?
+            <p className="text-danger">{errors.price.message}</p>
+            : ""}
+          <input className="m-2 form-control" type="text" onChange={(e) => setDescription(e.target.value)} />
+          {errors.description ?
+            <p className="text-danger">{errors.description.message}</p>
+            : ""}
+          <input className="m-2 btn btn-outline-dark" type="submit"/>
+        </form>
+      </div>
+    </div>
+    );
+}
+export default Form;
